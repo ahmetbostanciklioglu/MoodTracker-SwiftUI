d40c853e3bf98edd17d2f5100ce9de2d8d2ba44c
ahmetbostanciklioglu <ahmetbostancikli@gmail.com>
2024-12-01 13:55:40 +0300

    Initial Commit

---
+++ MoodTracker/MoodTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoodTrackerApp.swift
+//  MoodTracker
+//
+//  Created by Ahmet Bostancıklıoğlu on 1.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct MoodTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
